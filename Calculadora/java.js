ad50c86470ea1269e80fab4cb9a574d6253a0f0b
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-09-19 19:22:10 +0200

    Add files via upload

---
+++ Calculadora/java.js
@@ -0,0 +1,36 @@
+function calcularNumeros() {
+
+    let primerNumero = parseFloat(document.getElementById("numero1").value);
+    let segundoNumero = parseFloat(document.getElementById("numero2").value);
+    let operador = document.getElementById("operaciones").value;
+    let resultado;
+
+    if (isNaN(primerNumero) || isNaN(segundoNumero)) {
+        alert("El numero introducido no es valido o hay alguna casilla en blanco");
+        return;
+    }
+
+    switch (operador) {
+        case "1":
+            resultado = primerNumero + segundoNumero;
+            break;
+        case "2":
+            resultado = primerNumero - segundoNumero;
+            break;
+        case "3":
+            resultado = primerNumero * segundoNumero;
+            break;
+        case "4":
+            if (segundoNumero === 0) {
+                alert("No se puede dividir entre cero");
+                return;
+            }
+            resultado = primerNumero / segundoNumero;
+            break;
+        default:
+            alert("La operacion no es valida");
+            return;
+    }
+
+    document.getElementById("resul").innerHTML = resultado;
+}
