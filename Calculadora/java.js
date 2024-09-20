0beb9676acb73ba578b7905d62939713dfe9cfdd
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-09-20 09:51:09 +0200

    java.js

--- Calculadora/java.js
+++ Calculadora/java.js
@@ -10,27 +10,25 @@ function calcularNumeros() {
         return;
     }
 
-    switch (operador) {
-        case "1":
-            resultado = primerNumero + segundoNumero;
-            break;
-        case "2":
-            resultado = primerNumero - segundoNumero;
-            break;
-        case "3":
-            resultado = primerNumero * segundoNumero;
-            break;
-        case "4":
-            if (segundoNumero === 0) {
-                alert("No se puede dividir entre cero");
-                return;
-            }
-            resultado = primerNumero / segundoNumero;
-            break;
-        default:
-            alert("La operacion no es valida");
+    if(operador == "1"){
+        resultado = primerNumero + segundoNumero;
+
+    }else if(operador =="2"){
+        resultado = primerNumero - segundoNumero;
+    }else if(operador =="3"){
+        resultado = primerNumero * segundoNumero;
+    }else if(operador =="4"){
+        if (segundoNumero === 0) {
+            alert("No se puede dividir entre cero");
             return;
+        }
+        resultado = primerNumero / segundoNumero;
     }
+
+   
+
+    document.getElementById("resul").innerHTML = resultado;
+}
 
     document.getElementById("resul").innerHTML = resultado;
 }
